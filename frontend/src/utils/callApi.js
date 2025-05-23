6cae56bbff541e3e06e47ae5e2050d6a7f2465fe
Tibor Vito Susnjara <tibor.susnjara@student.um.si>
2025-05-23 21:18:17 +0200

    updated callApi to try refreshing token if 401 is returned

--- frontend/src/utils/callApi.js
+++ frontend/src/utils/callApi.js
@@ -11,8 +11,41 @@ async function callApi(method, url, endpoint, body, token) {
     options.headers = headers;
     options.method = method;
     const fetchResponse = await fetch(url + endpoint, options);
+    if (!fetchResponse.ok && fetchResponse.status == 401) {
+        try {
+            await refreshAccessToken(url);
+            options.headers["Authorization"] = "Bearer " + localStorage.getItem("accessToken");
+            const retryResponse = await fetch(url + endpoint, options);
+            const retryJson = await retryResponse.json();
+            if (retryResponse.ok) return retryJson;
+            else throw new Error("Invalid access token and cannot refresh");
+        }
+        catch (err) {
+            throw new Error("Invalid access token and cannot refresh");
+        }
+    }
     const responseJson = await fetchResponse.json();
     return responseJson;
+}
+
+async function refreshAccessToken(url) {
+    const refreshToken = localStorage.getItem("refreshToken");
+    if (!refreshToken) throw new Error("No refresh token available");
+
+    const fetchResponse = await fetch(url + "/auth/refresh", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            refreshToken: refreshToken
+        })
+    });
+    const responseJson = await fetchResponse.json();
+    if (!fetchResponse.ok) {
+        throw new Error("Error trying to refresh access token, response code " + fetchResponse.status + ", error: " + responseJson.error );
+    }
+    localStorage.setItem("accessToken", responseJson.accessToken);
 }
 
 export default callApi;
