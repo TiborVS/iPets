d5a0e0615bc339205ee9c4005356ea5d5f1a4059
Tibor Vito Susnjara <tibor.susnjara@student.um.si>
2025-06-01 21:11:20 +0200

    welcome page style

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router";
+import '../styles/Welcome.css';
 
 export default function Welcome() {
     
     return (
-        <div>
+        <div className="welcome">
             <h1>Dobrodošli v iPets</h1>
             <p>
                 Aplikacija za vodenje evidence o domačih živalih, ki
@@ -12,7 +13,7 @@ export default function Welcome() {
                 Predstavlja enotno mesto za vse podatke o živalih, tako da vam ni več treba imeti zapiske na različnih mestih, hraniti
                 izvide veterinarjev, nastavljati opomnike ali alarme za tablete itd.
             </p>
-            <p>Za uporabo se <Link to="/register">registrirajte</Link>, brezplačno je!</p>
+            <p>Za uporabo se <Link to="/register" style={{textDecoration: "underline"}}>registrirajte</Link>, brezplačno je!</p>
         </div>
     )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/global.css'
 import App from './App'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,7 +12,6 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>
 )
 
-/*
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/serviceWorker.js')
@@ -20,4 +19,3 @@ if ('serviceWorker' in navigator) {
             .catch(err => console.log("Service Worker registration failed", err));
     });
 }
-*/
